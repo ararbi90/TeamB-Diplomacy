3f7398bcdcf726e8260ca1042940d6aff1f93c0c
ararbi90 <ararbi90@hotmail.com>
2019-03-06 09:48:44 -0800

    Added the prototype GUI

--- SRC/team-b/main.js
+++ SRC/team-b/main.js
@@ -14,12 +14,10 @@ let win
 function createWindow() {
     // Create the browser window.
     win = new BrowserWindow({
-        width: 800,
-        height: 800
     })
 
     // and load the index.html of the app.
-    win.loadFile('src/index.html')
+    win.loadFile('src/game.html')
 
 
     // Open the DevTools.
