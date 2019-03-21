49be1df03036d1058917ca012b5e146548fcc358
Eli <eliaaron.gomez@gmail.com>
2019-03-20 21:00:55 -0700

    Dynamic dropdown placeholders
    'Choose...' now appears as a placeholder for each dropdown. If a item is chosen and the move is then changed, the placeholder will return. Placeholder is the first element of each array; perhaps not the best implementation but it works. Just make sure not to accept an order until all placeholders are removed via choices.

--- SRC/team-b/src/game.js
+++ SRC/team-b/src/game.js
@@ -22,15 +22,14 @@ document.getElementById("newGame").addEventListener("click", function(){
 });
 
 // Array of moves
-var moves = new Array("Hold", "Move", "Convoy", "Support");
+var moves = new Array("Choose...","Hold", "Move", "Convoy", "Support");
 // Array of units (TODO: pull the units from the database)
-var units = new Array("A PAR", "A BUR", "A PIC", "F ENG", "A BRE");
+var units = new Array("Choose...","A PAR", "A BUR", "A PIC", "F ENG", "A BRE");
 // Array of locations
-var locations = new Array("ADR","AEG","ALB","ANK","APU","ARM","BAL","BAR","BEL","BER","BLA","BOH","BRE","BUD","BUL","BUR","CLY","CON","DEN","EAS","EDI","ENG","FIN","GAL","GAS","GRE","LYO","BOT","HEL","HOL","ION","IRE","IRI","KIE","LVP","LVN","LON","MAR","MAO","MOS","MUN","NAP","NAO","NAF","NTH","NOR","NWG","PAR","PIC","PIE","POR","PRU","ROM","RUH","RUM","SER","SEV","SIL","SKA","SMY","SPA","STP","SWE","SWI","SYR","TRI","TUN","TUS","TYR","TYS","UKR","VEN","VIE","WAL","WAR","WES","YOR");
+var locations = new Array("Choose...","ADR","AEG","ALB","ANK","APU","ARM","BAL","BAR","BEL","BER","BLA","BOH","BRE","BUD","BUL","BUR","CLY","CON","DEN","EAS","EDI","ENG","FIN","GAL","GAS","GRE","LYO","BOT","HEL","HOL","ION","IRE","IRI","KIE","LVP","LVN","LON","MAR","MAO","MOS","MUN","NAP","NAO","NAF","NTH","NOR","NWG","PAR","PIC","PIE","POR","PRU","ROM","RUH","RUM","SER","SEV","SIL","SKA","SMY","SPA","STP","SWE","SWI","SYR","TRI","TUN","TUS","TYR","TYS","UKR","VEN","VIE","WAL","WAR","WES","YOR");
 
 // Get first move dropdown element from DOM
 var firstMoveDropdowns = document.getElementsByClassName("firstMoveSelect");
-var secondMoveDropdowns = document.getElementsByClassName("secondMoveSelect");
 
 // Loop through each first move dropdown
 for (var i = 0; i < firstMoveDropdowns.length; ++i) {
@@ -39,18 +38,7 @@ for (var i = 0; i < firstMoveDropdowns.length; ++i) {
         // Append the element to the end of Array list for the first move
         dropdown[dropdown.length] = new Option(moves[j], moves[j]);
     }
-}
-
-// Get location dropdown element from DOM
-var locationDropdowns = document.getElementsByClassName("locationSelect");
-
-// Loop through each location dropdown
-for (var i = 0; i < locationDropdowns.length; ++i) {
-    var dropdown = locationDropdowns[i];
-    for (var j = 0; j < locations.length; ++j) {
-        // Append the element to the end of the Array list
-        dropdown[dropdown.length] = new Option(locations[j], locations[j]);
-    }
+    dropdown.options[0].hidden = "true";
 }
 
 // https://stackoverflow.com/questions/3364493/how-do-i-clear-all-options-in-a-dropdown-box
@@ -63,17 +51,14 @@ function removeOptions(selectbox)
     }
 }
 
-function addUnitsToDropdown(move, index)
+function addUnitsToDropdown(move, unitDropdown)
 {
-    // Get dropdown by index
-    var unitDropdown = document.getElementById("unitSelect" + index);
-
     if (move == "Convoy")
     {
         for (var i = 0; i < units.length; ++i)
         {
             // Add only army units (can't convoy fleet)
-            if (units[i].charAt(0) === "A")
+            if (units[i].charAt(0) === "A" || units[i] === "Choose...")
             {
                 // Append the element to the end of the Array list
                 unitDropdown[unitDropdown.length] = new Option(units[i], units[i]);
@@ -89,19 +74,18 @@ function addUnitsToDropdown(move, index)
             unitDropdown[unitDropdown.length] = new Option(units[j], units[j]);
         }
     }
+
+    unitDropdown.options[0].hidden = "true";
 }
 
-function addSecondMovesToDropdown(move, index)
+function addSecondMovesToDropdown(move, secondMoveDropdown)
 {
-    // Get dropdown by index
-    var secondMoveDropdown = document.getElementById("secondMoveSelect" + index);
-
     if (move == "Convoy")
     {
         for (var i = 0; i < moves.length; ++i)
         {
             // Only valid second move for "Convoy" is "Move"
-            if (moves[i] === "Move")
+            if (moves[i] === "Move" || moves[i] === "Choose...")
             {
                 // Append the element to the end of Array list for the second move
                 secondMoveDropdown[secondMoveDropdown.length] = new Option(moves[i], moves[i]);
@@ -113,7 +97,7 @@ function addSecondMovesToDropdown(move, index)
         for (var i = 0; i < moves.length; ++i)
         {
             // Only valid second moves for "Support" is {"Move", "Hold"}
-            if (moves[i] === "Move" || moves[i] === "Hold")
+            if (moves[i] === "Move" || moves[i] === "Hold" || moves[i] === "Choose...")
             {
                 // Append the element to the end of Array list for the second move
                 secondMoveDropdown[secondMoveDropdown.length] = new Option(moves[i], moves[i]);
@@ -129,39 +113,54 @@ function addSecondMovesToDropdown(move, index)
             secondMoveDropdown[secondMoveDropdown.length] = new Option(moves[i], moves[i]);
         }
     }
+
+    secondMoveDropdown.options[0].hidden = "true";
+}
+
+function addLocationsToDropdown(locationDropdown)
+{   
+    for (var i = 0; i < locations.length; ++i)
+    {
+        // Append the element to the end of Array list
+        locationDropdown[locationDropdown.length] = new Option(locations[i], locations[i]);
+    }
+
+    locationDropdown.options[0].hidden = "true";
 }
 
 function firstMoveChoice(move, index) {
     var unitDropdown = document.getElementById("unitSelect" + index);
-    var secondMoveSelect = document.getElementById("secondMoveSelect" + index);
+    var secondMoveDropdown = document.getElementById("secondMoveSelect" + index);
     var locationDropdown = document.getElementById("locationSelect" + index);
 
     if (move == "Hold"){
         // Hide all other dropdowns
         unitDropdown.hidden = true;
-        secondMoveSelect.hidden =  true;
+        secondMoveDropdown.hidden =  true;
         locationDropdown.hidden = true;
     } else if (move == "Move"){
         // Show only location dropdown
         unitDropdown.hidden = true;
-        secondMoveSelect.hidden =  true;
+        secondMoveDropdown.hidden =  true;
         locationDropdown.hidden = false;
     } else if (move == "Convoy"){
         // Show all dropdowns
         unitDropdown.hidden = false;
-        secondMoveSelect.hidden =  false;
+        secondMoveDropdown.hidden =  false;
         locationDropdown.hidden = false;
     } else if (move == "Support"){
         // Show all dropdowns except for location
         unitDropdown.hidden = false;
-        secondMoveSelect.hidden =  false;
+        secondMoveDropdown.hidden =  false;
         locationDropdown.hidden = true;
     }
 
     removeOptions(unitDropdown);
-    removeOptions(secondMoveSelect);
-    addUnitsToDropdown(move, index);
-    addSecondMovesToDropdown(move, index);
+    removeOptions(secondMoveDropdown);
+    removeOptions(locationDropdown);
+    addUnitsToDropdown(move, unitDropdown);
+    addSecondMovesToDropdown(move, secondMoveDropdown);
+    addLocationsToDropdown(locationDropdown);
 }
 
 function secondMoveChoice(move, index)
@@ -179,4 +178,7 @@ function secondMoveChoice(move, index)
         // Show location dropdown
         locationDropdown.hidden = false;
     }
+
+    removeOptions(locationDropdown);
+    addLocationsToDropdown(locationDropdown);
 }
