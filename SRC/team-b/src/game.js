488ecb40eaab50a3764f642fa5c50bf1977aacc0
Eli <eliaaron.gomez@gmail.com>
2019-03-20 17:49:40 -0700

    Dynamic Dropdowns Update
    - If the user selects Support, only the unit and the second move is displayed. Can only support a Move or a Hold. If the second move is Move then display the location.
    - Can only convoy armies. Only valid second move for a convoy is Move.

--- SRC/team-b/src/game.js
+++ SRC/team-b/src/game.js
@@ -21,24 +21,27 @@ document.getElementById("newGame").addEventListener("click", function(){
 
 // Array of moves
 var moves = new Array("Hold", "Move", "Convoy", "Support");
-// Get dropdown element from DOM
-var moveDropdowns = document.getElementsByClassName("moveSelect");
+// Array of units (TODO: pull the units from the database)
+var units = new Array("A PAR", "A BUR", "A PIC", "F ENG", "A BRE");
+// Array of locations
+var locations = new Array("ADR","AEG","ALB","ANK","APU","ARM","BAL","BAR","BEL","BER","BLA","BOH","BRE","BUD","BUL","BUR","CLY","CON","DEN","EAS","EDI","ENG","FIN","GAL","GAS","GRE","LYO","BOT","HEL","HOL","ION","IRE","IRI","KIE","LVP","LVN","LON","MAR","MAO","MOS","MUN","NAP","NAO","NAF","NTH","NOR","NWG","PAR","PIC","PIE","POR","PRU","ROM","RUH","RUM","SER","SEV","SIL","SKA","SMY","SPA","STP","SWE","SWI","SYR","TRI","TUN","TUS","TYR","TYS","UKR","VEN","VIE","WAL","WAR","WES","YOR");
 
-// Loop through each dropdown
-for (var i = 0; i < moveDropdowns.length; ++i) {
-    var dropdown = moveDropdowns[i];
+// Get first move dropdown element from DOM
+var firstMoveDropdowns = document.getElementsByClassName("firstMoveSelect");
+
+// Loop through each first move dropdown
+for (var i = 0; i < firstMoveDropdowns.length; ++i) {
+    var dropdown = firstMoveDropdowns[i];
     for (var j = 0; j < moves.length; ++j) {
-        // Append the element to the end of Array list
+        // Append the element to the end of Array list for the first move
         dropdown[dropdown.length] = new Option(moves[j], moves[j]);
     }
 }
 
-// Array of locations
-var locations = new Array("ADR","AEG","ALB","ANK","APU","ARM","BAL","BAR","BEL","BER","BLA","BOH","BRE","BUD","BUL","BUR","CLY","CON","DEN","EAS","EDI","ENG","FIN","GAL","GAS","GRE","LYO","BOT","HEL","HOL","ION","IRE","IRI","KIE","LVP","LVN","LON","MAR","MAO","MOS","MUN","NAP","NAO","NAF","NTH","NOR","NWG","PAR","PIC","PIE","POR","PRU","ROM","RUH","RUM","SER","SEV","SIL","SKA","SMY","SPA","STP","SWE","SWI","SYR","TRI","TUN","TUS","TYR","TYS","UKR","VEN","VIE","WAL","WAR","WES","YOR");
-// Get dropdown element from DOM
+// Get location dropdown element from DOM
 var locationDropdowns = document.getElementsByClassName("locationSelect");
 
-// Loop through each dropdown
+// Loop through each location dropdown
 for (var i = 0; i < locationDropdowns.length; ++i) {
     var dropdown = locationDropdowns[i];
     for (var j = 0; j < locations.length; ++j) {
@@ -47,46 +50,149 @@ for (var i = 0; i < locationDropdowns.length; ++i) {
     }
 }
 
-// Array of units
-// TODO: pull the units from the database
-var units = new Array("A PAR", "A BUR", "A PIC", "F ENG", "A BRE");
-// Get dropdown element from DOM
-var unitDropdowns = document.getElementsByClassName("unitSelect");
-
-// Loop through each dropdown
-for (var i = 0; i < unitDropdowns.length; ++i) {
-    var dropdown = unitDropdowns[i];
-    for (var j = 0; j < units.length; ++j) {
-        // Append the element to the end of the Array list
-        dropdown[dropdown.length] = new Option(units[j], units[j]);
+// https://stackoverflow.com/questions/3364493/how-do-i-clear-all-options-in-a-dropdown-box
+function removeOptions(selectbox)
+{
+    var i;
+    for (i = selectbox.options.length - 1 ; i >= 0 ; i--)
+    {
+        selectbox.remove(i);
     }
 }
 
-function moveChoice(move, index) {
-    var unitDropdown = document.getElementById("unitSelect" + index);
-    var locationDropdown = document.getElementById("locationSelect" + index);
-    var moveSelect = document.getElementById("moveSelect" + index + "b");
+function addUnitsToDropdown(move)
+{
+    // Get dropdown element from DOM
+    var unitDropdowns = document.getElementsByClassName("unitSelect");
 
-    if(move=="Hold"){
-        //Hide all other dropdowns
+    if (move == "Convoy")
+    {
+        for (var i = 0; i < unitDropdowns.length; i++)
+        {
+            var dropdown = unitDropdowns[i];
+            for (var j = 0; j < units.length; ++j)
+            {
+                // Add only army units (can't convoy fleet)
+                if (units[j].charAt(0) === "A")
+                {
+                    // Append the element to the end of the Array list
+                    dropdown[dropdown.length] = new Option(units[j], units[j]);
+                }
+            }
+        }
+    }
+    else
+    {
+        // Otherwise add all units
+        for (var i = 0; i < unitDropdowns.length; i++)
+        {
+            var dropdown = unitDropdowns[i];
+            for (var j = 0; j < units.length; ++j)
+            {
+                // Append the element to the end of the Array list
+                dropdown[dropdown.length] = new Option(units[j], units[j]);
+            }
+        }
+    }
+}
+
+function addSecondMovesToDropdown(move)
+{
+    var moveDropdowns2 = document.getElementsByClassName("secondMoveSelect");
+
+    if (move == "Convoy")
+    {
+        for (var i = 0; i < moveDropdowns2.length; ++i)
+        {
+            var dropdown = moveDropdowns2[i];
+            for (var j = 0; j < moves.length; ++j)
+            {
+                // Only valid second move for "Convoy" is "Move"
+                if (moves[j] === "Move")
+                {
+                    // Append the element to the end of Array list for the second move
+                    dropdown[dropdown.length] = new Option(moves[j], moves[j]);
+                }
+            }
+        }
+    }
+    else if (move == "Support")
+    {
+        for (var i = 0; i < moveDropdowns2.length; ++i)
+        {
+            var dropdown = moveDropdowns2[i];
+            for (var j = 0; j < moves.length; ++j)
+            {
+                // Only valid second moves for "Support" is {"Move", "Hold"}
+                if (moves[j] === "Move" || moves[j] === "Hold")
+                {
+                    // Append the element to the end of Array list for the second move
+                    dropdown[dropdown.length] = new Option(moves[j], moves[j]);
+                }
+            }
+        }
+    }
+    else
+    {
+        // Otherwise add all options (it will be hidden)
+        for (var i = 0; i < moveDropdowns2.length; ++i)
+        {
+            var dropdown = moveDropdowns2[i];
+            for (var j = 0; j < moves.length; ++j)
+            {
+                // Append the element to the end of Array list for the second move
+                dropdown[dropdown.length] = new Option(moves[j], moves[j]);
+            }
+        }
+    }
+}
+
+function firstMoveChoice(move, index) {
+    var unitDropdown = document.getElementById("unitSelect" + index);
+    var secondMoveSelect = document.getElementById("secondMoveSelect" + index);
+    var locationDropdown = document.getElementById("locationSelect" + index);
+
+    if (move == "Hold"){
+        // Hide all other dropdowns
         unitDropdown.hidden = true;
+        secondMoveSelect.hidden =  true;
         locationDropdown.hidden = true;
-        moveSelect.hidden =  true;
-        
-    } else if(move == "Move"){
-        //Show only location dropdown
+    } else if (move == "Move"){
+        // Show only location dropdown
         unitDropdown.hidden = true;
+        secondMoveSelect.hidden =  true;
         locationDropdown.hidden = false;
-        moveSelect.hidden =  true;
-    } else if(move == "Convoy"){
-        //Show all dropdowns
+    } else if (move == "Convoy"){
+        // Show all dropdowns
         unitDropdown.hidden = false;
+        secondMoveSelect.hidden =  false;
         locationDropdown.hidden = false;
-        moveSelect.hidden =  false;
-    } else if(move == "Support"){
-        //Show all dropdowns
+    } else if (move == "Support"){
+        // Show all dropdowns except for location
         unitDropdown.hidden = false;
+        secondMoveSelect.hidden =  false;
+        locationDropdown.hidden = true;
+    }
+
+    removeOptions(unitDropdown);
+    removeOptions(secondMoveSelect);
+    addUnitsToDropdown(move);
+    addSecondMovesToDropdown(move);
+}
+
+function secondMoveChoice(move, index)
+{
+    var locationDropdown = document.getElementById("locationSelect" + index);
+
+    if (move == "Hold")
+    {
+        // Hide location dropdown
+        locationDropdown.hidden = true;
+        
+    } 
+    else if (move == "Move")
+    {
+        // Show location dropdown
         locationDropdown.hidden = false;
-        moveSelect.hidden =  false;
     }
 }
