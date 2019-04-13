de06bdedc03a5f04e5b2d2bc32ea20e5e79253e7
ararbi90 <ararbi90@hotmail.com>
2019-04-12 19:24:12 -0700

    made minor changes

--- SRC/team-b/src/js/invite.js
+++ SRC/team-b/src/js/invite.js
@@ -206,7 +206,6 @@ $(document).ready(function () {
                                 $("#invitePage").hide();
                                 $("#gemeCreated").show();
 
-
                             }
                         })
 
