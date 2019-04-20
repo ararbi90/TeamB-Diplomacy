80ea3723bfd0ff2ab0e856790b3ccb53fb650579
ararbi90 <ararbi90@hotmail.com>
2019-04-20 15:32:08 -0700

    saves

--- SRC/firebase/functions/routes/users.js
+++ SRC/firebase/functions/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
+
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  admin.database().ref('player').once('value').then(snapshot =>{
+    data = {};
+    snapshot.forEach(r =>{
+      data[r.key] = r.val();
+    })
+    res.send(data);
+    return true;
+  }).catch(error => {console.log(error)});
 });
 
 module.exports = router;
